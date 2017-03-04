f0e8b458a0672142e28ce0ef98b56fad82f417a2
Keionne Derousselle <Keionne.Derousselle@extendhealth.com>
2017-03-03 17:18:15 -0700

    Adding some nicer react

--- OdeToFood/src/OdeToFood/Components/App.jsx
+++ OdeToFood/src/OdeToFood/Components/App.jsx
@@ -1,26 +1,54 @@
 ﻿import React, {Component} from 'react';
 import 'whatwg-fetch';
 
+function ErrorMessage(props) {
+    return <h2>{props.errorMessage}</h2>;
+}
+
+function RestaurantsContent(props) {
+    let restaurants = props.restaurants.map((restaurant) =>
+        <li key={restaurant.id}>{restaurant.name}</li>
+        );
+
+    return(
+        <div>
+            <h2>{props.currentMessage}</h2>
+            <ul>{restaurants}</ul>
+        </div>
+    );
+}
+
+function AppContent(props) {
+    const hasErrored = props.errorMessage;
+    if (hasErrored) {
+        return <ErrorMessage errorMessage={props.errorMessage}/>;
+    }
+    return <RestaurantsContent currentMessage={props.currentMessage} restaurants={props.restaurants}/>;
+}
+
+
 export default class App extends Component{
     
     constructor(props){
         super(props);
+
         this.state = {
-            currentState: "",
+            currentMessage: "",
             restaurants: [],
             responseErrorMessage: ""
         }
     }
 
     componentDidMount() {
-        fetch('http://localhost:55257/home/getrestaurantsfg')
+        fetch('http://localhost:55257/home/getrestaurants')
             .then((response) => {
-                if (response.status === 200) {
-                    var data = JSON.parse(response.text());
-                    this.setState(data);
-                } else {
+                if (response.status !== 200) {
                     this.setState({ responseErrorMessage: response.statusText });
                 }
+                return response.text();
+            }).then((body) => {
+                const data = JSON.parse(body);
+                this.setState(data);
             });
     }
 
@@ -29,16 +57,12 @@ export default class App extends Component{
     }
 
     render(){
-        let restaurants = this.state.restaurants.map((restaurant) =>
-            <li key={restaurant.id}>{restaurant.name}</li>
-        );
-
-        let errorMessage = this.state.responseErrorMessage;
-
-        return ( 
+       return ( 
             <div>
-                <h3> {this.state.currentState} </h3>
-                <ul>{restaurants}</ul>
+               <AppContent 
+                    errorMessage={this.state.responseErrorMessage}
+                    currentMessage ={this.state.currentMessage}
+                    restaurants = {this.state.restaurants} />
             </div>
             );
     }
